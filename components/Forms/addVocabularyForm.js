28292595fd7af6282f1be0113de4b579c0c4441a
Jesse Ramirez <gideonramirezz@gmail.com>
2023-12-04 21:28:29 -0600

    added nav bar

---
+++ components/Forms/addVocabularyForm.js
@@ -0,0 +1,28 @@
+import clearDom from '../../utils/clearDom';
+import renderToDOM from '../../utils/renderToDom';
+
+const addVocabForm = (obj = {}) => {
+  clearDom();
+  // Safe check for firebaseKey
+  const firebaseKey = obj ? obj.firebaseKey : null;
+  const domString = `
+    <form id="${firebaseKey ? `update-vocab--${firebaseKey}` : 'submit-vocab'}" class="mb-4">
+      <div class="form-group">
+        <label for="image">Title</label>
+        <input type="text" class="form-control" id="title" placeholder="Title" required>
+      </div>
+      <div class="form-group">
+        <label for="image">Deffinition</label>
+        <input type="text" class="form-control" id="deffinition" placeholder="Deffinition" required>
+      </div>
+      <div class="form-group">
+        <label for="title">Language/Tech</label>
+        <input type="text" class="language-tech" id="language/tech" aria-describedby="language" placeholder="Enter Email" required>
+      </div>
+      <button type="submit" class="btn btn-primary mt-3">Submit </button>
+    </form>`;
+
+  renderToDOM('#form-container', domString);
+};
+
+export default addVocabForm;
