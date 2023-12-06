2ada55ac56cab97df65993641b4d79ed303c87e1
Jesse Ramirez <gideonramirezz@gmail.com>
2023-12-06 16:48:23 -0600

    working through addding functonality to the form

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,6 +1,11 @@
-const domEvents = () => {
-  document.querySelector('#main-container').addEventListener('click', () => {
+import addVocabForm from '../components/Forms/addVocabularyForm';
 
+const domEvents = () => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    if (e.target.id === 'add-vocabulary-btn') {
+      e.preventDefault();
+      addVocabForm();
+    }
   });
 };
 export default domEvents;
