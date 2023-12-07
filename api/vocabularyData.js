3f1090434dfa90387597f642bdd5e33018d0d99f
Jesse Ramirez <gideonramirezz@gmail.com>
2023-12-07 13:10:13 -0600

    got the app to display the data from fireBase

--- api/vocabularyData.js
+++ api/vocabularyData.js
@@ -1,5 +1,4 @@
 // import endpoint from '../utils/client';
-
 const getVocabulary = (uid) => new Promise((resolve, reject) => {
   fetch(`https://vocab-you-lary-f2729-default-rtdb.firebaseio.com/vocabulary.json?uid=${uid}`, {
     method: 'GET',
@@ -8,15 +7,7 @@ const getVocabulary = (uid) => new Promise((resolve, reject) => {
     },
   })
     .then((response) => response.json())
-    .then((data) => {
-      const vocabularyArray = [{
-        title: data.title,
-        definition: data.definition,
-        language: data.language,
-        firebaseKey: data.firebaseKey
-      }];
-      resolve(vocabularyArray);
-    })
+    .then((data) => resolve(Object.values(data)))
     .catch(reject);
 });
 const createVocabulary = (payload) => new Promise((resolve, reject) => {
